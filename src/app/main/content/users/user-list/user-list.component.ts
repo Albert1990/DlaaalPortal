dac0ffec0cefc9506d4e5e7fe4e97642b5aa1689
halaelba <hala.albarshah@gmail.com>
2018-02-12 14:29:51 +0200

    changes -some validations, some styles -deactivate user - new user, user details,

--- src/app/main/content/users/user-list/user-list.component.ts
+++ src/app/main/content/users/user-list/user-list.component.ts
@@ -1,19 +1,19 @@
-import { AppSettings } from './../../shared/app.settings';
-import { UserType } from './../enums/user-type';
-import { UserStatus } from './../enums/user-status';
-import { Subscription } from 'rxjs/Subscription';
-import { UsersService } from './../users.service';
-import { FuseSplashScreenService } from './../../../../core/services/splash-screen.service';
-import { FuseConfirmDialogComponent } from './../../../../core/components/confirm-dialog/confirm-dialog.component';
-import { MatDialogRef, MatDialog, MatPaginator, MatTableDataSource, PageEvent } from '@angular/material';
-import { FormControl } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { fuseAnimations } from './../../../../core/animations';
-import { Observable } from 'rxjs/Observable';
-import { User } from './../user.model';
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
-import { ActivatedRouteSnapshot } from '@angular/router/src/router_state';
+import {AppSettings} from './../../shared/app.settings';
+import {UserType} from './../enums/user-type';
+import {UserStatus} from './../enums/user-status';
+import {Subscription} from 'rxjs/Subscription';
+import {UsersService} from './../users.service';
+import {FuseSplashScreenService} from './../../../../core/services/splash-screen.service';
+import {FuseConfirmDialogComponent} from './../../../../core/components/confirm-dialog/confirm-dialog.component';
+import {MatDialogRef, MatDialog, MatPaginator, MatTableDataSource, PageEvent} from '@angular/material';
+import {FormControl} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {fuseAnimations} from './../../../../core/animations';
+import {Observable} from 'rxjs/Observable';
+import {User} from './../user.model';
+import {Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
+import {DataSource} from '@angular/cdk/collections';
+import {ActivatedRouteSnapshot} from '@angular/router/src/router_state';
 
 @Component({
   selector: 'app-user-list',
@@ -34,10 +34,10 @@ export class UserListComponent implements OnInit, OnDestroy {
   startedWith: string = '';
 
   constructor(private usersService: UsersService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private dialog: MatDialog,
-    private loadingScreen: FuseSplashScreenService) {
+              private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private dialog: MatDialog,
+              private loadingScreen: FuseSplashScreenService) {
   }
 
   ngOnInit() {
@@ -49,7 +49,7 @@ export class UserListComponent implements OnInit, OnDestroy {
     this.onPageChangeSubscription = this.paginator.page.subscribe(
       (pageEvent: PageEvent) => {
         // make http request to get users in pageIndex: pageEvent.index
-        //this.usersService.listing(pageEvent.pageIndex, pageEvent.pageSize, this.startedWith)
+        // this.usersService.listing(pageEvent.pageIndex, pageEvent.pageSize, this.startedWith)
       }
     );
   }
@@ -58,8 +58,11 @@ export class UserListComponent implements OnInit, OnDestroy {
     this.onPageChangeSubscription.unsubscribe();
   }
 
-  editItem(itemId: string) {
-    this.router.navigate(['/users', itemId, 'edit']);
+  routeTo(item, to: string) {
+    if (to === 'delete')
+      this.deleteItem(item);
+    else
+       this.router.navigate(['/users', item.id, to]);
   }
 
   applyFilter(startedWith: string) {
@@ -69,32 +72,34 @@ export class UserListComponent implements OnInit, OnDestroy {
       this.usersService.listing(this.paginator.pageIndex,
         this.paginator.pageSize,
         startedWith).then(serverResult => {
-          console.log(serverResult);
-        }).catch(reason => {
-          console.log('error while filtering data');
-        });
+        console.log(serverResult);
+      }).catch(reason => {
+        console.log('error while filtering data');
+      });
     }
   }
 
-  deleteItem(itemId: string) {
+  deleteItem(item: string) {
     this.confirmDialogRef = this.dialog.open(FuseConfirmDialogComponent, {
       disableClose: false
     });
 
-    this.confirmDialogRef.componentInstance.confirmMessage = 'Are you sure you want to delete?';
+    this.confirmDialogRef.componentInstance.confirmMessage = 'Are you sure you want to deactivate the user?';
 
     this.confirmDialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.loadingScreen.show();
-        this.usersService.delete(itemId).then(
+        console.log("item ", item);
+        // this.loadingScreen.show();
+        this.usersService.delete(item).then(
           (serverResult) => {
-            this.loadingScreen.hide();
-            this.dataSource.data = serverResult.users;
-            this.resultsLength = serverResult.totalCount;
+            console.log('delete user serverResult! ', serverResult);
+            //  this.loadingScreen.hide();
+            // this.dataSource.data = serverResult.users;
+            // this.resultsLength = serverResult.totalCount;
           },
           (reason) => {
-            this.loadingScreen.hide();
-            console.log('delete user error!');
+            // this.loadingScreen.hide();
+            console.log('delete user error! ', reason);
           }
         );
       }
