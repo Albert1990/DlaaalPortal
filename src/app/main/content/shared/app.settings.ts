cc14babdffdff2713b71f1b5b5c53f06eef9ae38
halaelba <hala.albarshah@gmail.com>
2018-10-18 09:42:25 +0200

    Change the API

--- src/app/main/content/shared/app.settings.ts
+++ src/app/main/content/shared/app.settings.ts
@@ -1,5 +1,6 @@
 
 export class AppSettings {
     // public static baseUrl: string = 'http://104.217.253.15:7500/api';
-    public static baseUrl: string = 'http://108.179.218.237:7500/api';
+    // public static baseUrl: string = 'http://108.179.218.237:7500/api';
+    public static baseUrl: string = "https://dlaaal.com/api";
 }
